f29ffc74e48d7639cdd88867110debacb0f48e88
Patryk Pocica <patryk.pocica23@gmail.com>
2026-04-28 09:19:50 +0200

    Dodano renderowanie koszyka

---
+++ products.js
@@ -0,0 +1,6 @@
+export const products = [
+    { id: 1, name: "Chleb pszenny", price: 5.50 },
+    { id: 2, name: "Masło ekstra", price: 7.20 },
+    { id: 3, name: "Mleko 3.2%", price: 3.80 },
+    { id: 4, name: "Jajka (10 szt.)", price: 9.00 }
+];
